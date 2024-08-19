e50a992066ef49507ae0a7fe36274c6e5ff6ae2e
nansu0425 <nansu0425@gmail.com>
2024-08-19 21:59:24 +0900

    Ex0502 완료

--- Ex0502_Josephus/Ex0502_Josephus.cpp
+++ Ex0502_Josephus/Ex0502_Josephus.cpp
@@ -17,15 +17,23 @@ int main()
 	q.Print();
 
 	// 마지막 한 명이 남을 때까지 반복
+	while (q.Size() > 1)
 	{
 		// k-1 명은 맨 앞에서 뒤로 보내기
+		for (int count = 0; count < k - 1; ++count)
+		{
+			int num = q.Front();
+			q.Enqueue(num);
+			q.Dequeue();
+		}
 
 		// k 번째 사람 처형
-		// cout << "Executed " << ... << endl;
+		cout << "Executed " << q.Front() << endl;
+		q.Dequeue();
 	}
 
 	// 최후의 생존자 번호
-	// cout << "Survivor: " << ... << endl;
+	cout << "Survivor: " << q.Front() << endl;
 
 	return 0;
 }
