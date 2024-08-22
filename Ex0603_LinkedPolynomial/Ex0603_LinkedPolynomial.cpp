2c0aa5ab97a2db1d43958f510e65111ae364e815
nansu0425 <nansu0425@gmail.com>
2024-08-22 20:42:10 +0900

    Ex0603 완료

--- Ex0603_LinkedPolynomial/Ex0603_LinkedPolynomial.cpp
+++ Ex0603_LinkedPolynomial/Ex0603_LinkedPolynomial.cpp
@@ -12,11 +12,48 @@ struct Term
 class LinkedPolynomial : public SinglyLinkedList<Term>
 {
 public:
-	typedef SinglyLinkedList<Term>::Node Node;
+	using Node = SinglyLinkedList<Term>::Node;
 
 	void NewTerm(float coef, int exp)
 	{
-		// TODO:
+		// 노드가 없는 경우
+		if (first_ == nullptr)
+		{
+			first_ = new Node{ {coef, exp}, nullptr };
+			return;
+		}
+
+		// 첫 번째 항 앞에 삽입하는 경우
+		if (exp < first_->item.exp)
+		{
+			first_ = new Node{ {coef, exp}, first_ };
+			return;
+		}
+		// 첫 번째 항과 차수가 같은 경우
+		else if (exp == first_->item.exp)
+		{
+			first_->item.coef += coef;
+			return;
+		}
+
+		Node* cur = first_;
+
+		// cur으로 삽입할 위치를 순차적으로 탐색
+		while (cur->next && (cur->next->item.exp < exp))
+		{
+			cur = cur->next;
+		}
+
+		// 추가하려는 항의 차수와 같은 차수의 항인 경우
+		if (cur->next && (cur->next->item.exp == exp))
+		{
+			cur->next->item.coef += coef;
+		}
+		// cur에 해당하는 항 뒤에 새로운 항을 삽입해야 한다
+		else
+		{
+			cur->next = new Node{ {coef, exp}, cur->next };
+		}
 	}
 
 	float Eval(float x)
@@ -24,6 +61,13 @@ public:
 		float temp = 0.0f;
 
 		// TODO:
+		Node* cur = first_;
+
+		while (cur)
+		{
+			temp += cur->item.coef * std::powf(x, cur->item.exp);
+			cur = cur->next;
+		}
 
 		return temp;
 	}
@@ -35,10 +79,44 @@ public:
 
 		LinkedPolynomial temp;
 
-		Node* i = this->first_;
-		Node* j = poly.first_;
+		Node* thisCur = this->first_;
+		Node* paramCur = poly.first_;
 
-		// TODO:
+		while (thisCur && paramCur)
+		{
+			if (thisCur->item.exp < paramCur->item.exp)
+			{
+				temp.PushBack(thisCur->item);
+				thisCur = thisCur->next;
+			}
+			else if (thisCur->item.exp > paramCur->item.exp)
+			{
+				temp.PushBack(paramCur->item);
+				paramCur = paramCur->next;
+			}
+			else
+			{
+				temp.PushBack({ thisCur->item.coef + paramCur->item.coef, thisCur->item.exp });
+				thisCur = thisCur->next;
+				paramCur = paramCur->next;
+			}
+		}
+
+		assert((thisCur == nullptr) || (paramCur == nullptr));
+
+		while (thisCur)
+		{
+			temp.PushBack(thisCur->item);
+			thisCur = thisCur->next;
+		}
+
+		while (paramCur)
+		{
+			temp.PushBack(paramCur->item);
+			paramCur = paramCur->next;
+		}
+
+		assert((thisCur == nullptr) && (paramCur == nullptr));
 
 		return temp;
 	}
@@ -47,7 +125,28 @@ public:
 	{
 		bool is_first = true; // 더하기 출력시 확인용
 
-		// TODO:
+		Node* cur = first_;
+
+		while (cur)
+		{
+			if (is_first)
+			{
+				std::cout << cur->item.coef;
+
+				if (cur->item.exp > 0)
+				{
+					std::cout << "x^" << cur->item.exp;
+				}
+
+				is_first = false;
+			}
+			else
+			{
+				std::cout << " + " << cur->item.coef << "x^" << cur->item.exp;
+			}
+
+			cur = cur->next;
+		}
 
 		cout << endl;
 	}
@@ -61,11 +160,10 @@ int main()
 
 	LinkedPolynomial p1;
 
-	// exp가 작은 항부터 추가한다고 가정
-	p1.NewTerm(1.0f, 0);	// 1 * x^0 = 1
-	p1.NewTerm(1.5f, 1);	// 1.5 * x^1
 	p1.NewTerm(2.0f, 2);	// 2 * x^2
-
+	p1.NewTerm(1.5f, 1);	// 1.5 * x^1
+	p1.NewTerm(1.0f, 0);	// 1 * x^0 = 1
+	
 	p1.Print(); // 1 + 1.5*x^1 + 2*x^2
 
 	cout << p1.Eval(0.0f) << endl; // 1 + 1.5*0 + 2*0^2 = 1
@@ -109,24 +207,22 @@ int main()
 	{
 		LinkedPolynomial p1; // max_degree는 기본값으로 설정
 
-		// exp가 작은 항부터 추가한다고 가정
-		p1.NewTerm(1.0f, 0);
-		p1.NewTerm(1.5f, 1);
-		p1.NewTerm(2.0f, 2);
-		p1.NewTerm(5.0f, 7);
 		p1.NewTerm(3.5f, 10);
 		p1.NewTerm(5.5f, 20);
+		p1.NewTerm(1.0f, 0);
 		p1.NewTerm(5.0f, 1000);
+		p1.NewTerm(2.0f, 2);
+		p1.NewTerm(5.0f, 7);
+		p1.NewTerm(1.5f, 1);
 
 		p1.Print(); // 1 + 1.5*x^1 + 2*x^2 + 5*x^7 + 3.5*x^10 + 5.5*x^20 + 5*x^1000
 
 		LinkedPolynomial p2;
 
-		// exp가 작은 항부터 추가한다고 가정
+		p2.NewTerm(2.0f, 11);
+		p2.NewTerm(3.0f, 2);
 		p2.NewTerm(3.2f, 0);
 		p2.NewTerm(1.0f, 1);
-		p2.NewTerm(3.0f, 2);
-		p2.NewTerm(2.0f, 11);
 
 		p2.Print(); // 3.2 + 1*x^1 + 3*x^2 + 2*x^11
 
