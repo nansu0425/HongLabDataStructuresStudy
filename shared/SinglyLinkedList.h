0875361e2df85a58ab7b9bfaaaf9f64af4494e2a
nansu0425 <nansu0425@gmail.com>
2024-08-22 18:54:23 +0900

    Ex0602 완료

--- shared/SinglyLinkedList.h
+++ shared/SinglyLinkedList.h
@@ -20,6 +20,17 @@ public:
 	SinglyLinkedList(const SinglyLinkedList& list)
 	{
 		// TODO: 연결 리스트 복사
+		if (list.first_)
+		{
+			first_ = new Node(*list.first_);
+			Node* cur = first_;
+
+			while (cur->next)
+			{
+				cur->next = new Node(*(cur->next));
+				cur = cur->next;
+			}
+		}
 	}
 
 	~SinglyLinkedList()
@@ -30,6 +41,14 @@ public:
 	void Clear() // 모두 지워야(delete) 합니다.
 	{
 		// TODO: 모두 삭제
+		Node* del = first_;
+
+		while (del)
+		{
+			Node* prevDel = del;
+			del = del->next;
+			delete prevDel;
+		}
 	}
 
 	bool IsEmpty()
@@ -42,6 +61,13 @@ public:
 		int size = 0;
 
 		// TODO: size를 하나하나 세어서 반환
+		Node* cur = first_;
+
+		while (cur)
+		{
+			++size;
+			cur = cur->next;
+		}
 
 		return size;
 	}
@@ -50,19 +76,38 @@ public:
 	{
 		assert(first_);
 
-		return T(); // TODO: 수정
+		return first_->item; // TODO: 수정
 	}
 
 	T Back()
 	{
 		assert(first_);
 
-		return T(); // TODO: 수정
+		Node* cur = first_;
+
+		while (cur->next)
+		{
+			cur = cur->next;
+		}
+
+		return cur->item; // TODO: 수정
 	}
 
 	Node* Find(T item)
 	{
 		// TODO: item이 동일한 노드 포인터 반환
+
+		Node* cur = first_;
+
+		while (cur)
+		{
+			if (cur->item == item)
+			{
+				return cur;
+			}
+
+			cur = cur->next;
+		}
 
 		return nullptr;
 	}
@@ -70,25 +115,45 @@ public:
 	void InsertBack(Node* node, T item)
 	{
 		// TODO:
+		if (node)
+		{
+			node->next = new Node{ item, node->next };
+		}
 	}
 
 	void Remove(Node* n)
 	{
-		assert(first_);
+		assert(first_ && n);
 
 		// 하나 앞의 노드를 찾아야 합니다.
 		// TODO:
+		if (n == first_)
+		{
+			first_ = first_->next;
+		}
+		else
+		{
+			Node* prevDel = first_;
+
+			while (prevDel && prevDel->next != n)
+			{
+				prevDel = prevDel->next;
+			}
+
+			assert(prevDel);
+
+			prevDel->next = n->next;
+		}
+
+		delete n;
 	}
 
 	void PushFront(T item)
 	{
 		// first_가 nullptr인 경우와 아닌 경우 나눠서 생각해보기 (결국은 두 경우를 하나로 합칠 수 있음)
 
-		// 새로운 노드 만들기
-		// TODO:
-
-		// 연결 관계 정리
-		// TODO:
+		Node* newFirst = new Node{ item, first_ };
+		first_ = newFirst;
 	}
 
 	void PushBack(T item)
@@ -96,10 +161,19 @@ public:
 		if (first_)
 		{
 			// TODO:
+			Node* last = first_;
+
+			while (last->next)
+			{
+				last = last->next;
+			}
+
+			last->next = new Node{ item, nullptr };
 		}
 		else
 		{
 			// TODO:
+			PushFront(item);
 		}
 	}
 
@@ -115,6 +189,7 @@ public:
 		assert(first_);
 
 		// TODO: 메모리 삭제
+		Remove(first_);
 	}
 
 	void PopBack()
@@ -131,11 +206,41 @@ public:
 		assert(first_);
 
 		// TODO: 메모리 삭제
+		if (first_->next == nullptr)
+		{
+			Remove(first_);
+		}
+		else
+		{
+			Node* prevDel = first_;
+
+			while (prevDel->next->next)
+			{
+				prevDel = prevDel->next;
+			}
+
+			delete prevDel->next;
+			prevDel->next = nullptr;
+		}
 	}
 
 	void Reverse()
 	{
 		// TODO: 
+		if (first_)
+		{
+			Node* newNext = nullptr;
+
+			while (first_)
+			{
+				Node* oldNext = first_->next;
+				first_->next = newNext;
+				newNext = first_;
+				first_ = oldNext;
+			}
+
+			first_ = newNext;
+		}
 	}
 
 	void SetPrintDebug(bool flag)
