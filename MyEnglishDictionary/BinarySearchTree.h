d3dd6078385e79c052a304b187141b6a37c9b142
nansu0425 <nansu0425@gmail.com>
2024-09-03 02:26:08 +0900

    Node 구조체 템플릿 추가

--- MyEnglishDictionary/BinarySearchTree.h
+++ MyEnglishDictionary/BinarySearchTree.h
@@ -1,6 +1,17 @@
 #pragma once
 
+template<typename K, typename V>
+struct Node
+{
+	K		key;
+	V		value;
+	int		height = 0;
+	Node*	pLeft = nullptr;
+	Node*	pRight = nullptr;
+};
+
 template<typename K, typename V> 
 class BinarySearchTree
 {
+
 };
