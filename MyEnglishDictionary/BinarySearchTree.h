298f3b1ebc015a1ddc63fbf02ec3c427c3eeb02e
nansu0425 <nansu0425@gmail.com>
2024-09-03 15:50:47 +0900

    Node 구조체 생성자 추가, BinarySearchTree 메서드 추가

--- MyEnglishDictionary/BinarySearchTree.h
+++ MyEnglishDictionary/BinarySearchTree.h
@@ -1,17 +1,47 @@
 #pragma once
 
-template<typename K, typename V>
+#include <utility>
+
+template<typename Key, typename Value>
 struct Node
 {
-	K		key;
-	V		value;
+	Key		key;
+	Value	value;
 	int		height = 0;
 	Node*	pLeft = nullptr;
 	Node*	pRight = nullptr;
+
+	Node() = default;
+	Node(Node&& other) = default;
+
+	Node(const Key& key, const Value& value)
+		: key(key)
+		, value(value)
+	{ }
+
+	Node(Key&& key, Value&& value) noexcept
+		: key(std::move(key))
+		, value(std::move(value))
+	{ }
 };
 
-template<typename K, typename V> 
+template<typename Key, typename Value> 
 class BinarySearchTree
 {
+	using BstNode = Node<Key, Value>;
 
+public:
+	~BinarySearchTree();
+
+	template<typename Node>
+	void	insert(Node&& node);
+	void	remove(const Key& key);
+	
+	const BstNode*	getPtrNode const (const Key& key);
+
+private:
+	void	deleteAllNodes(BstNode* pRoot);
+
+private:
+	BstNode*	m_pRoot = nullptr;
 };
