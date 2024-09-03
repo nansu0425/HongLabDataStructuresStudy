a0ee641ecce890435f255f32dc09776ad51b1db0
nansu0425 <nansu0425@gmail.com>
2024-09-03 15:49:26 +0900

    이동 생성자, 버퍼 접근 메서드 추가

--- MyEnglishDictionary/String.h
+++ MyEnglishDictionary/String.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <fstream>
+#include <algorithm>
+#include <utility>
 
 const int g_bufSize = 1024;
 
@@ -10,7 +12,12 @@ public:
 	String()
 	{
 		m_pBuf = new char[g_bufSize];
+		std::fill(m_pBuf, m_pBuf + g_bufSize, 0);
 	}
+
+	String(String&& other) noexcept
+		: m_pBuf(std::exchange(other.m_pBuf, nullptr))
+	{ }
 
 	~String()
 	{
@@ -20,6 +27,7 @@ public:
 		}
 	}
 
+	char*	getPtrBuf() { return m_pBuf; }
 
 private:
 	char*		m_pBuf = nullptr;
