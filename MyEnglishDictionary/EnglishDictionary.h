501e790e09e1afecf61d8c46180b796d313458c9
nansu0425 <nansu0425@gmail.com>
2024-09-04 21:51:30 +0900

    m_wordSearchTree 멤버와 set 메서드 추가

--- MyEnglishDictionary/EnglishDictionary.h
+++ MyEnglishDictionary/EnglishDictionary.h
@@ -1,5 +1,13 @@
 ﻿#pragma once
 
+#include "String.h"
+#include "BinarySearchTree.h"
+
 class EnglishDictionary
 {
+public:
+	void	setWordSearchTree(const char* fileName);
+
+private:
+	BinarySearchTree<String, String>	m_wordSearchTree;
 };
