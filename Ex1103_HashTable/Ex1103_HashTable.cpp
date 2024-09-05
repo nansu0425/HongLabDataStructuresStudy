2b00a5395d89142b16add6edac2e7b54613e2e56
nansu0425 <nansu0425@gmail.com>
2024-09-05 17:41:59 +0900

    Ex1103 완료

--- Ex1103_HashTable/Ex1103_HashTable.cpp
+++ Ex1103_HashTable/Ex1103_HashTable.cpp
@@ -24,36 +24,101 @@ public:
 		delete[] table_;
 	}
 
+	// Quadratic probing
 	void Insert(const Item& item)
 	{
-		// TODO:
+		if (numKeys_ == capacity_)
+		{
+			std::cout << "Failed to insert: table is full" << std::endl;
+			return;
+		}
 
-		size_t index = item.key; // 키를 인덱스로 사용
-		table_[index] = item;
+		size_t index = HashFunc(item.key) % capacity_;
+
+		for (int count = 1; count <= capacity_; ++count)
+		{
+			// 슬롯에 key가 없는 경우
+			if (table_[index].key == K())
+			{
+				table_[index] = item;
+				++numKeys_;
+
+				return;
+			}
+			// 같은 key가 있는 경우
+			if (table_[index].key == item.key)
+			{
+				table_[index].value = item.value;
+
+				return;
+			}
+
+			index = (index + count * count) % capacity_;
+		}
+
+		std::cout << "Failed to insert: available slot isn't found" << std::endl;
 	}
 
 	V Get(const K& key)
 	{
-		// TODO: 못 찾으면 0을 반환
+		if (numKeys_ == 0)
+		{
+			std::cout << "Failed to get: table is empty" << std::endl;
+			return V();
+		}
 
-		size_t index = key;
-		return table_[index].value;
+		size_t index = HashFunc(key) % capacity_;
+ 
+		for (int count = 1; count <= capacity_; ++count)
+		{
+			// key가 같은 경우
+			if (table_[index].key == key)
+			{
+				return table_[index].value;
+			}
+			// key가 없는 경우
+			if (table_[index].key == K())
+			{
+				break;
+			}
+
+			index = (index + count * count) % capacity_;
+		}
+
+		std::cout << "Failed to get: key isn't found" << std::endl;
+
+		return V();
 	}
 
 	// 정수 -> 해시값
-	size_t HashFunc(const int& key)
+	// Folding method
+	size_t HashFunc(int key)
 	{
-		// TODO:
+		size_t hashVal = 0;
 
-		return key;
+		while (key > 0)
+		{
+			hashVal += key % 1000;
+			key /= 1000;
+		}
+
+		return hashVal;
 	}
 
 	// 문자열을 정수 인덱스(해시값)로 변환
 	// Horner's method
-	//size_t HashFunc(const string& s)
-	//{
-	//  return TODO:
-	//}
+	size_t HashFunc(const string& s)
+	{
+		size_t hashVal = 0;
+		int c = 31;
+
+		for (int idx = 0; idx < s.size(); ++idx)
+		{
+			hashVal = c * hashVal + s.at(idx);
+		}
+
+		return hashVal;
+	}
 
 	void Print()
 	{
@@ -65,6 +130,7 @@ public:
 private:
 	Item* table_ = nullptr;
 	int capacity_ = 0;
+	int numKeys_ = 0;
 };
 
 int main()
@@ -75,58 +141,58 @@ int main()
 
 	// 키: int, 값: int 인 경우
 	// 키의 범위가 아주 크고 아이템의 개수는 적을 경우
-	{
-		using Item = HashTable<int, int>::Item;
+	//{
+	//	using Item = HashTable<int, int>::Item;
 
-		HashTable<int, int> h(8);
+	//	HashTable<int, int> h(8);
 
-		h.Insert(Item{ 123, 456 });
+	//	h.Insert(Item{ 123, 456 });
 
-		h.Print();
+	//	h.Print();
 
-		cout << "Get 123 " << h.Get(123) << endl;
+	//	cout << "Get 123 " << h.Get(123) << endl;
 
-		h.Insert(Item{ 1000021, 9898 });
+	//	h.Insert(Item{ 1000021, 9898 });
 
-		h.Print();
+	//	h.Print();
 
-		cout << "Get 1000021 " << h.Get(1000021) << endl;
+	//	cout << "Get 1000021 " << h.Get(1000021) << endl;
 
-		h.Insert(Item{ 1211, 999 }); // 충돌!
+	//	h.Insert(Item{ 1211, 999 }); // 충돌!
 
-		h.Print();
+	//	h.Print();
 
-		cout << "Get 123 " << h.Get(123) << endl;
-		cout << "Get 1211 " << h.Get(1211) << endl;
-	}
+	//	cout << "Get 123 " << h.Get(123) << endl;
+	//	cout << "Get 1211 " << h.Get(1211) << endl;
+	//}
 
 	// 키: std::string, 값: int
-	//{
-	//	using Item = HashTable<string, int>::Item;
+	{
+		using Item = HashTable<string, int>::Item;
 
-	//	HashTable<string, int> h(8);
+		HashTable<string, int> h(8);
 
-	//	h.Insert(Item{ "apple", 1 });
-	//	h.Insert(Item{ "orange", 2 });
-	//	h.Insert(Item{ "mandarin", 4 });
+		h.Insert(Item{ "apple", 1 });
+		h.Insert(Item{ "orange", 2 });
+		h.Insert(Item{ "mandarin", 4 });
 
-	//	h.Print();
+		h.Print();
 
-	//	cout << "apple " << h.Get("apple") << endl;
-	//	cout << "orange " << h.Get("orange") << endl;
-	//	cout << endl;
+		cout << "apple " << h.Get("apple") << endl;
+		cout << "orange " << h.Get("orange") << endl;
+		cout << endl;
 
-	//	h.Print();
+		h.Print();
 
-	//	h.Insert(Item{ "tomato", 4 });
+		h.Insert(Item{ "tomato", 4 });
 
-	//	h.Print(); 
+		h.Print(); 
 
-	//	cout << "apple " << h.Get("apple") << endl;
-	//	cout << "orange " << h.Get("orange") << endl;
-	//	cout << "pineapple " << h.Get("pineapple") << endl;
-	//	cout << endl;
-	//}
+		cout << "apple " << h.Get("apple") << endl;
+		cout << "orange " << h.Get("orange") << endl;
+		cout << "pineapple " << h.Get("pineapple") << endl;
+		cout << endl;
+	}
 
 	return 0;
 }
