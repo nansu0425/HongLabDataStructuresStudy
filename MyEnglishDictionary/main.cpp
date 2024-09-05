1e5dc650c236ec4ccb4c88cf93e50016800b0858
nansu0425 <nansu0425@gmail.com>
2024-09-06 01:09:51 +0900

    메인 로직 구현

--- MyEnglishDictionary/main.cpp
+++ MyEnglishDictionary/main.cpp
@@ -1,8 +1,21 @@
+﻿#include "EnglishDictionary.h"
+
 #include <iostream>
 
 int main()
 {
+	EnglishDictionary enDict;
+	enDict.setWordSearchTree("eng_dic.txt");
 
+	String word;
+
+	while (std::cin.good() == true)
+	{
+		std::cout << "\nInput a word: ";
+		word.getLine(std::cin);
+
+		std::cout << "Meaning: " << enDict.search(word) << std::endl;
+	}
 
 	return 0;
 }
